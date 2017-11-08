0c14f17c63904433634b48d6e2857203827efdf1
guowei yang <guowei yang>
2017-11-09 01:02:53 +0800

    d

--- Exp2/RAM_UART/FreqDiv.vhd
+++ Exp2/RAM_UART/FreqDiv.vhd
@@ -18,24 +18,42 @@
 --
 ----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.std_logic_1164.all;
 
 entity FreqDiv is
+    generic
+    (
+        div : integer := 50;
+        half : integer := 25 
+    );
+    port
+    (
+        CLK: in std_logic;
+        RST: in std_logic;
+        O: out std_logic
+    );
 end FreqDiv;
 
-architecture Behavioral of FreqDiv is
-
+architecture FreqDiv_bhv of FreqDiv is
+    signal counter: integer range 0 to half - 1;
+    signal output: std_logic;    
 begin
+    O <= output;
 
-
-end Behavioral;
-
+    process(CLK, RST)
+    begin
+        if RST = '1' then
+            output <= '0';
+            counter <= 0;
+        else
+            if rising_edge(CLK) then
+                if counter = half - 1 then
+                    output <= not output;
+                    counter <= 0;
+                else
+                    counter <= counter + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+end;
--- Exp2/RAM_UART/main.vhd
+++ Exp2/RAM_UART/main.vhd
@@ -17,15 +17,12 @@
 -- Additional Comments: 
 --
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-use work.constant.ALL;
+library IEEE;use IEEE.STD_LOGIC_1164.ALL;use IEEE.NUMERIC_STD.ALL;use IEEE.STD_LOGIC_ARITH.ALL;use IEEE.std_logic_unsigned.all;use work.constants.ALL;
 
 entity main is
     Port ( i_Click 			: in  	STD_LOGIC;
+			  i_Clock         : in  	STD_LOGIC;
            i_nReset 			: in  	STD_LOGIC;
-           i_Key0				: in  	STD_LOGIC;
 			  o_Led				: out		STD_LOGIC_VECTOR (WORD_WIDTH-1 downto 0);
            
 			  o_SysBus_Addr	: out  	STD_LOGIC_VECTOR (ADDR_WIDTH-1 downto 0);
@@ -45,13 +42,15 @@ entity main is
 			  
 			  UART_nWE			: out		STD_LOGIC;
 			  UART_TBRE			: in		STD_LOGIC;
-			  UART_TSRE			: in		STD_LOGIC  
+			  UART_TSRE			: in		STD_LOGIC
 			);
  end main;
 
 architecture Behavioral of main is
-	type t_State is (s_Init, s1, s2, s3, s4);
-	type t_SubState is (s_R1_Read_Setup, s_R1_Read_Datain);
+	type type_State is (s_Init, s1, s2, s3, s4);
+	type type_SubState is (s_R1_Read_Setup, s_R1_Read_Datain);
+	type type_SubState4 is (s_40, s_41, s_42, s_43, s_44);
+   signal t_State: type_State;
 	signal clock : STD_LOGIC;
 	
 	component FreqDiv is
@@ -81,41 +80,71 @@ begin
         O => clock
     );
 
-	process (i_clk, i_rst)
+	process (clock, i_nReset)
 		variable t_addr : STD_LOGIC_VECTOR (ADDR_WIDTH-1 downto 0);
 		variable t_data : STD_LOGIC_VECTOR (WORD_WIDTH-1 downto 0);
-		variable r_i    : integer range 0 to 31; 
+		variable r_i    : integer range 0 to 31;
+		variable st_ygw : integer range 0 to 31;
+		variable cnt_ygw : integer range 0 to 31;
 		variable l_Click : STD_LOGIC;
 	begin
 		if i_nReset = '0' then
 			-- reset
 			t_State <= s_Init;
+			
 		elsif falling_edge(clock) then
 			case t_State is
 				-- r_i := 0;
 				-- t_SubState <= s_R1_Write
-				when s2 =>
-					case t_SubState <= s_R1_Read_Setup
-					o_SysBus_Data <= t_data;
-					o_SysBus_Addr <= t_addr;
-					o_Ram1_nCE <= '1';
-					o_Ram1_nWE <= '0';
-					o_Ram1_nOE <= '1';
-					o_Led(15 downto 8) <= t_addr(7 downto 0);
-					o_Led(7 downto 0) <= t_data(7 downto 0);
-					if r_i < 10 then
-						if r_i > 0 then
-							t_data := t_data + 1;
-							t_addr := t_addr + 1;
-						end if;
-						r_i := r_i + 1;
-						t_SubState <= s_R1_Write;
-						t_State <= s2;
+				when s_Init =>
+					if l_Click='0' and i_Click='1' then
+						t_State <= s1;
+						cnt_ygw := 0;
+						st_ygw := 0;
 					end if;
-						when s_R1_
+					
+				when s1 =>
+					case st_ygw is
+						when 0 =>
+							if l_Click='0' and i_Click='1' then
+								st_ygw := 1;
+								cnt_ygw := cnt_ygw+1;
+							end if;
+						when 1 =>
+							st_ygw := 2;
+							i_UART_nRE <= '1';
+							o_SysBus_Data <= (others => 'Z');
+						when 2 =>
+							if i_UART_Ready='1' then
+								st_ygw := 3;
+							else
+								st_ygw := 1;
+							end if;
+						when 3 =>
+							st_ygw := 0;
+							case cnt_ygw is
+								when 1 =>
+									t_addr(15 downto 8) := o_SysBus_Data(7 downto 0);
+									o_Led(7 downto 0) <= o_SysBus_Data(7 downto 0);
+								when 2 =>
+									t_addr(7 downto 0) := o_SysBus_Data(7 downto 0);
+									o_Led(7 downto 0) <= o_SysBus_Data(7 downto 0);
+								when 3 =>
+									t_data(15 downto 8) := o_SysBus_Data(7 downto 0);
+									o_Led(7 downto 0) <= o_SysBus_Data(7 downto 0);
+								when 4 =>
+									t_data(7 downto 0) := o_SysBus_Data(7 downto 0);
+									o_Led(7 downto 0) <= o_SysBus_Data(7 downto 0);
+									t_State <= s2;
+									i_UART_nRE <= '1';
+								when others =>
+									st_ygw := 0;						
+							end case;
+						when others =>
+							st_ygw := 0;
 					end case;
-				when s3 =>
-					t_State <= s_R
+				when others =>
+					st_ygw := 0;
 			end case;
 			l_Click := i_Click;
 		end if;
