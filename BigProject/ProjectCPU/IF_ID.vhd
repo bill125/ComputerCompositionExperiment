f1a64fc750d6bc6fc9b549843c59604f5b55d897
lazycal <lazycal12@gmail.com>
2017-11-24 19:47:55 +0800

    Add some constants and IF_ID and PC.

--- BigProject/ProjectCPU/IF_ID.vhd
+++ BigProject/ProjectCPU/IF_ID.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use workspace.inst_constants;
+use workspace.op_type_constants;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,12 +32,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity IF_ID is
+    port (
+        i_clock : in std_logic;
+        i_inst : in std_logic_vector(15 downto 0);
+        i_PC : in std_logic_vector(15 downto 0);
+        i_stall : in std_logic;
+        i_clear : in std_logic;
+        o_OP : out std_logic_vector(4 downto 0);
+        o_inst : out std_logic_vector(15 downto 0);
+        o_rxAddr : out std_logic_vector(2 downto 0);
+        o_ryAddr : out std_logic_vector(2 downto 0)
+    );
 end IF_ID;
 
 architecture Behavioral of IF_ID is
-
 begin
-
-
+    process(i_clock)
+    begin
+        if rising_edge(i_clock) then
+            if i_clear = '1' then
+                o_OP <= workspace.inst_constants.OP_NOP;
+                o_inst <= workspace.inst_constants.INST_NOP;
+                o_rxAddr <= '1' & workspace.op_type_constants.invalid;
+                o_ryAddr <= '1' & workspace.op_type_constants.invalid;
+            elsif i_stall = '0' then
+                o_OP <= i_inst(15 downto 11);
+                o_inst <= i_inst;
+                o_rxAddr <= '0' & i_inst(10 downto 8);
+                o_ryAddr <= '0' & i_inst(10 downto 8);
+            end if;
+        end if;
+    end process
 end Behavioral;
 
---
+++ BigProject/ProjectCPU/inst_const.vhd
@@ -0,0 +1,38 @@
+library IEEE;
+use workspace.constants.op_t;
+use IEEE.std_logic_1164.all;
+
+package inst_const is
+    constant OP_ADDIU: std_logic_vector(5 downto 0) := "01001";
+    constant OP_ADDIU3: std_logic_vector(5 downto 0) := "01000";
+    constant OP_ADDSP: std_logic_vector(5 downto 0) := "01100";
+    constant OP_ADDU: std_logic_vector(5 downto 0) := "11100";
+    constant OP_AND: std_logic_vector(5 downto 0) := "11101";
+    constant OP_B: std_logic_vector(5 downto 0) := "00010";
+    constant OP_BEQZ: std_logic_vector(5 downto 0) := "00100";
+    constant OP_BNEZ: std_logic_vector(5 downto 0) := "00101";
+    constant OP_BTEQZ: std_logic_vector(5 downto 0) := "01100";
+    constant OP_CMP: std_logic_vector(5 downto 0) := "11101";
+    constant OP_JR: std_logic_vector(5 downto 0) := "11101";
+    constant OP_LI: std_logic_vector(5 downto 0) := "01101";
+    constant OP_LW: std_logic_vector(5 downto 0) := "10011";
+    constant OP_LW_SP: std_logic_vector(5 downto 0) := "10010";
+    constant OP_MFIH: std_logic_vector(5 downto 0) := "11110";
+    constant OP_MFPC: std_logic_vector(5 downto 0) := "11101";
+    constant OP_MOVE: std_logic_vector(5 downto 0) := "01111";
+    constant OP_MTIH: std_logic_vector(5 downto 0) := "11110";
+    constant OP_MTSP: std_logic_vector(5 downto 0) := "01100";
+    constant OP_NEG: std_logic_vector(5 downto 0) := "11101";
+    constant OP_NOP: std_logic_vector(5 downto 0) := "00001";
+    constant INST_NOP: std_logic_vector(15 downto 0) := "0000100000000000";
+    constant OP_NOT: std_logic_vector(5 downto 0) := "11101";
+    constant OP_OR: std_logic_vector(5 downto 0) := "11101";
+    constant OP_SLL: std_logic_vector(5 downto 0) := "00110";
+    constant OP_SLT: std_logic_vector(5 downto 0) := "11101";
+    constant OP_SLTUI: std_logic_vector(5 downto 0) := "01011";
+    constant OP_SRA: std_logic_vector(5 downto 0) := "00110";
+    constant OP_SUBU: std_logic_vector(5 downto 0) := "11100";
+    constant OP_SW: std_logic_vector(5 downto 0) := "11011";
+    constant OP_SW_SP: std_logic_vector(5 downto 0) := "11010";
+
+end;
