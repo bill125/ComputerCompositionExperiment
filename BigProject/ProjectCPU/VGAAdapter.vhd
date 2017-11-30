2566722d81581b5be6a1a0d075a8332de11df175
bill125 <bill125@gmail.com>
2017-12-01 02:24:21 +0800

    Fix VGAAdapter.

--- BigProject/ProjectCPU/VGAAdapter.vhd
+++ BigProject/ProjectCPU/VGAAdapter.vhd
@@ -19,13 +19,9 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.constants.all;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use work.constants.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -52,20 +48,20 @@ entity VGAAdapter is
 end VGAAdapter;
 
 architecture Behavioral of VGAAdapter is
-    signal x : integer range from 0 to 1023 := 0;
-    signal y : integer range from 0 to 1023 := 0;
+    signal x : integer range 0 to 1023 := 0;
+    signal y : integer range 0 to 1023 := 0;
 begin
     o_busRequest.writeRequest <= '0';
     o_busRequest.readRequest <= i_read_EN;
     o_busRequest.data <= (others => '-');
 
     o_data <= i_busResponse.data;
-    x <= to_integer(i_vectorX);
-    y <= to_integer(i_vectorY);
+    x <= conv_integer(i_vectorX);
+    y <= conv_integer(i_vectorY);
 
     process (x, y)
     begin
-        o_busRequest.data = conv_std_logic_vector(screenW * (y - screenY) + x - screenX);
+        o_busRequest.data <= conv_std_logic_vector(screenW * (y - screenY) + x - screenX, 16);
     end process;
 
 end Behavioral;
