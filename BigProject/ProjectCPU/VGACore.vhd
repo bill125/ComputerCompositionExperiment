b9d2cf758fa9e630751f31d97c6b15376ee7f557
guowei yang <guowei yang>
2017-12-01 22:32:10 +0800

    fix bug of VGA

--- BigProject/ProjectCPU/VGACore.vhd
+++ BigProject/ProjectCPU/VGACore.vhd
@@ -22,16 +22,17 @@ library	ieee;
 use		ieee.std_logic_1164.all;
 use		ieee.std_logic_unsigned.all;
 use		ieee.std_logic_arith.all;
+use     ieee.numeric_std.all;
 
 entity VGACore is
 	 port(
 			clk : in  std_logic;
-			i_data : in std_logic_vector(15 downto 0); --读到的数据
+			i_data : in std_logic_vector(15 downto 0); --读到的数�
 			hs,vs : out std_logic;
 			r,g,b : out std_logic_vector(2 downto 0);
 			o_vectorX : out std_logic_vector(9 downto 0);  --需要获取颜色的x
 			o_vectorY : out std_logic_vector(8 downto 0);  --需要获取颜色的y
-			o_read_EN : out std_logic -- 是否需要读SRAM ‘1’-读 ; '0' - 不读
+			o_read_EN : out std_logic -- 是否需要读SRAM ��� '0' - 不读
 	  );
 end VGACore; 
 
@@ -40,15 +41,21 @@ architecture behavior of VGACore is
 	signal hs1,vs1    : std_logic;				
 	signal vector_x : std_logic_vector(9 downto 0);
 	signal vector_y : std_logic_vector(8 downto 0);
+	signal unsignedA : unsigned(9 downto 0);
+	--signal unsignedB : unsigned(8 downto 0);
 begin
 	 process(clk)
 	 begin
 	  	if clk'event and clk='1' then
 	   		if vector_x=799 then
 	    		vector_x <= (others=>'0');
-				o_read_EN <= '1';
-	   		else
+	   		else 
 	    		vector_x <= vector_x + 1;
+				if vector_x=159 then
+					o_read_EN <= '1';
+				elsif vector_x=479 then
+					o_read_EN <= '0';
+				end if;
 	   		end if;
 	  	end if;
 	 end process;
@@ -61,6 +68,9 @@ begin
 					o_read_EN <= '1';
 	    		else
 	     			vector_y <= vector_y + 1;
+					if vector_y=480 then
+						o_read_EN <= '0';
+					end if;
 	    		end if;
 	   		end if;
 	  	end if;
@@ -97,16 +107,19 @@ begin
 	   		vs <=  vs1;
 	  	end if;
 	 end process;
- 	o_vectorX <= vector_x;
- 	o_vectorY <= vector_y;
+
+ 	unsignedA <= unsigned(vector_x);
+ 	o_vectorX <= to_stdlogicvector(to_bitvector(conv_std_logic_vector(unsignedA-160,10)) srl 1) when unsignedA>159 else
+	 			 (others => '0');
+ 	o_vectorY <= to_stdlogicvector(to_bitvector(vector_y) srl 1);
+	
 	process(clk,vector_x,vector_y)
 	begin
 		if(clk'event and clk='1')then
-			if vector_x > 639 or vector_y > 479 then
+			if vector_x < 160 or vector_x > 479 or vector_y > 479 then
 					r1  <= "000";
 					g1	<= "000";
 					b1	<= "000";
-					o_read_EN <= '0';
 			else
 				r1 <= i_data(15 downto 13);
 				g1 <= i_data(12 downto 10);
