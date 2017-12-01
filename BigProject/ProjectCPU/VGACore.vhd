7073bdff9e4c760bd1f88717ffba72b65c3b2e13
guowei yang <guowei yang>
2017-12-01 13:46:10 +0800

    VGAcore

--- BigProject/ProjectCPU/VGACore.vhd
+++ BigProject/ProjectCPU/VGACore.vhd
@@ -25,11 +25,13 @@ use		ieee.std_logic_arith.all;
 
 entity VGACore is
 	 port(
-			clk_0 : in  std_logic;
+			clk : in  std_logic;
+			i_data : in std_logic_vector(15 downto 0); --读到的数据
 			hs,vs : out std_logic;
 			r,g,b : out std_logic_vector(2 downto 0);
-			GRamAddra : in std_logic_vector(15 downto 0);
-			GRamDina : in std_logic_vector(15 downto 0)
+			o_vectorX : out std_logic_vector(9 downto 0);  --需要获取颜色的x
+			o_vectorY : out std_logic_vector(8 downto 0);  --需要获取颜色的y
+			o_read_EN : out std_logic -- 是否需要读SRAM ‘1’-读 ; '0' - 不读
 	  );
 end VGACore; 
 
@@ -38,53 +40,13 @@ architecture behavior of VGACore is
 	signal hs1,vs1    : std_logic;				
 	signal vector_x : std_logic_vector(9 downto 0);
 	signal vector_y : std_logic_vector(8 downto 0);
-	signal clk	:	 std_logic;
-	signal GRamAddrb_in : std_logic_vector(18 downto 0);
-	signal GRamDoutb_in :  std_logic_vector(0 downto 0);
-	signal unsignedA : unsigned(9 downto 0);
-	signal unsignedB : unsigned(8 downto 0);
-	signal result : unsigned(18 downto 0);
-	signal GRamDouta : std_logic_vector(15 downto 0);
-	signal const40 : std_logic_vector(9 downto 0);
-	component GM is
-		port (
-			clka : in std_logic;
-			wea : in std_logic_vector(0 downto 0);
-			addra : in std_logic_vector(14 downto 0);
-			dina : in std_logic_vector(15 downto 0);
-			douta : out std_logic_vector(15 downto 0);
-			clkb : in std_logic;
-			web : in std_logic_vector(0 downto 0);
-			addrb : in std_logic_vector(18 downto 0);
-			dinb : in std_logic_vector(0 downto 0);
-			doutb : out std_logic_vector(0 downto 0)
-		);
-	end component;
 begin
-	const40 <= "101000";
-	GRam_c: GRam port map(
-			clka => clk_0,
-			clkb => clk_0,
-			wea => "1",
-			web => "0",
-			addra => GRamAddra,
-			dina => GRamDina,
-			douta => GRamDouta,
-			addrb => GRamAddrb_in,
-			dinb => (others => 'Z'),
-			doutb => GRamDoutb_in
-		);
-  	 process(clk_0)
-     begin
-        if(clk_0'event and clk_0='1') then 
-             clk <= not clk;
-        end if;
- 	 end process;
 	 process(clk)
 	 begin
 	  	if clk'event and clk='1' then
 	   		if vector_x=799 then
 	    		vector_x <= (others=>'0');
+				o_read_EN <= '1';
 	   		else
 	    		vector_x <= vector_x + 1;
 	   		end if;
@@ -96,6 +58,7 @@ begin
 	   		if vector_x=799 then
 	    		if vector_y=524 then
 	     			vector_y <= (others=>'0');
+					o_read_EN <= '1';
 	    		else
 	     			vector_y <= vector_y + 1;
 	    		end if;
@@ -134,21 +97,20 @@ begin
 	   		vs <=  vs1;
 	  	end if;
 	 end process;
- 	unsignedA <= unsigned(vector_x) SRA 2;
- 	unsignedB <= unsigned(vector_y) SRA 2;
- 	result <= unsignedB*unsigned(const40)+unsignedA;
- 	GRamAddrb_in <= std_logic_vector(result);
+ 	o_vectorX <= vector_x;
+ 	o_vectorY <= vector_y;
 	process(clk,vector_x,vector_y)
 	begin
 		if(clk'event and clk='1')then
-			if vector_x > 639 or vector_y > 479 then 
+			if vector_x > 639 or vector_y > 479 then
 					r1  <= "000";
 					g1	<= "000";
 					b1	<= "000";
+					o_read_EN <= '0';
 			else
-				r1 <= GRamDoutb_in(15 downto 13);
-				g1 <= GRamDoutb_in(12 downto 10);
-				b1 <= GRamDoutb_in(9 downto 7);
+				r1 <= i_data(15 downto 13);
+				g1 <= i_data(12 downto 10);
+				b1 <= i_data(9 downto 7);
 			end if;
 		end if;		 
 	end process;	
