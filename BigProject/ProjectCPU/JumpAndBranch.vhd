446ba958cd1816fe6b0996b5fffaaa6f3bd14072
lazycal <lazycal12@gmail.com>
2017-12-02 02:01:51 +0800

    Fix btb less one bug.

--- BigProject/ProjectCPU/JumpAndBranch.vhd
+++ BigProject/ProjectCPU/JumpAndBranch.vhd
@@ -47,9 +47,7 @@ entity JumpAndBranch is
 end JumpAndBranch;
 
 architecture Behavioral of JumpAndBranch is
-
 begin
-
     process (i_OP0, i_OP1, i_imm, i_OP)
     begin
         case i_OP is
@@ -62,7 +60,7 @@ begin
                     o_jumpTarget <= i_OP1 + i_imm;
                 else
                     o_jumpEN <= '0';
-                    o_jumpTarget <= i_OP1 + '1';
+                    o_jumpTarget <= i_OP1;
                 end if;
             when op_BNEZ => -- if rx /= 0 then PC = PC + imm
                 if i_OP0 /= x"0000" then
@@ -70,14 +68,14 @@ begin
                     o_jumpTarget <= i_OP1 + i_imm;
                 else
                     o_jumpEN <= '0';
-                    o_jumpTarget <= i_OP1 + '1';
+                    o_jumpTarget <= i_OP1;
                 end if;
             when op_JR =>
                 o_jumpEN <= '1';
                 o_jumpTarget <= i_OP0;
             when others =>
                 o_jumpEN <= '0';
-                o_jumpTarget <= i_OP1 + '1';
+                o_jumpTarget <= i_OP1;
         end case;
     end process;
 
